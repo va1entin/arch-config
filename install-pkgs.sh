dec30dd2e3fb2747e9b4b63cbe01ed073628481b
va1entin <valh@mailbox.org>
2021-04-20 15:15:31 +0200

    add install-pkgs.sh

---
+++ install-pkgs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Upgrading packages..."
+pacman -Syu
+echo ""
+
+echo "Iterating package lists..."
+find pkgs/ -type f -name "*.list" -exec bash -c '
+    read -n1 -p "Install packages from {} [y/n] " yn
+    case "$yn" in
+        [Yy]*)
+            echo -e "\n\t Installing packages from {}"
+            pacman -S - < {}
+            echo ""
+            ;; \
+        *)
+            echo -e "\n\t Not installing packages from {}"
+            echo ""
+            ;;
+    esac' \;
