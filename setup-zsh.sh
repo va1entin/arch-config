20fbce4ebaba2fb458826593b92ec00ba5c222da
Valentin Heidelberger <valh@mailbox.org>
2021-04-20 17:25:26 +0200

    separate zsh setup

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-ZSHRC_PATH="~/.zshrc"
+ZSHRC_PATH=~/.zshrc
 
 echo "Installing zsh and curl..."
-pacman -S zsh curl
+sudo pacman -S zsh curl
 echo ""
 
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo ""
-
-echo "Setting theme in ${ZSHRC_PATH}..."
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="maran"/' "${ZSHRC_PATH}"
-echo ""
-
-echo "Setting plugins in ${ZSHRC_PATH}..."
-sed -i 's/^plugins=.*/plugins=(git)/' "${ZSHRC_PATH}"
-echo ""
-
-echo "Adding aliases to ${ZSHRC_PATH}..."
-echo -e "\n# Aliases" >> "${ZSHRC_PATH}"
-cat configs/zshrc >> "${ZSHRC_PATH}"
 echo ""
