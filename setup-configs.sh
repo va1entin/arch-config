500ba43eac6758fe440725790d77dacf73b615ad
Valentin Heidelberger <valh@mailbox.org>
2021-04-20 19:55:09 +0200

    fix path in setup-configs.sh

--- setup-configs.sh
+++ setup-configs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-ln -s configs/vimrc ~/.vimrc
+ln -s ~/git/arch-config/configs/vimrc ~/.vimrc
 
 echo "set -g default-shell /usr/bin/zsh" > ~/.byobu/.tmux.conf
 echo "set -g default-command /usr/bin/zsh" >> ~/.byobu/.tmux.conf
