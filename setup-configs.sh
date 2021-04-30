c438dafec1ca56d8e7330ab65914206fb802400f
Valentin Heidelberger <valh@mailbox.org>
2021-04-30 18:22:56 +0200

    add info output

--- setup-configs.sh
+++ setup-configs.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 
+echo "Creating symlink for vimrc..."
 ln -s ~/git/arch-config/configs/vimrc ~/.vimrc
+echo ""
 
+echo "Setting zsh as default shell for byobu..."
 echo "set -g default-shell /usr/bin/zsh" > ~/.byobu/.tmux.conf
 echo "set -g default-command /usr/bin/zsh" >> ~/.byobu/.tmux.conf
+echo ""
--- setup-syncthing.sh
+++ setup-syncthing.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Installing syncthing..."
 sudo pacman -S syncthing
+echo ""
 
+echo "Enabling syncthing user service..."
 systemctl enable --user syncthing.service
+echo ""
+
+echo "Starting syncthing user service..."
 systemctl start --user syncthing.service
+echo ""
