819a3dca5d0e9aa2764f6dfe3558c726324d7413
Valentin Heidelberger <valh@mailbox.org>
2021-05-05 18:50:31 +0200

    add setup-soft-brightness.sh

---
+++ setup-soft-brightness.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SB_VERSION=27
+
+echo "Downloading soft-brightness version ${SB_VERSION}..."
+wget "https://github.com/F-i-f/soft-brightness/releases/download/v${SB_VERSION}/soft-brightness@fifi.org.v${SB_VERSION}.shell-extension.zip"
+echo ""
+
+echo "Creating extension directory..."
+mkdir ~/.local/share/gnome-shell/extensions/soft-brightness@fifi.org
+echo ""
+
+echo "Unzipping soft-brightness..."
+unzip -q "soft-brightness@fifi.org.v${SB_VERSION}.shell-extension.zip" -d ~/.local/share/gnome-shell/extensions/soft-brightness@fifi.org
+echo ""
+
+echo "Enabling soft-brightness with gnome-extensions..."
+gnome-extensions enable soft-brightness@fifi.org
+echo ""
