71e41a60b10fc770738a0fc1bada01c72d78a07a
Valentin Heidelberger <valh@mailbox.org>
2021-05-12 13:37:47 +0200

    Revert "missing sudo"
    This reverts commit 607b5c75825ebc5aa1fd44bca76331de79f01936.

--- setup-syncthing.sh
+++ setup-syncthing.sh
@@ -5,9 +5,9 @@ sudo pacman -S syncthing
 echo ""
 
 echo "Enabling syncthing user service..."
-sudo systemctl enable --user syncthing.service
+systemctl enable --user syncthing.service
 echo ""
 
 echo "Starting syncthing user service..."
-sudo systemctl start --user syncthing.service
+systemctl start --user syncthing.service
 echo ""
