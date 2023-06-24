8a7578252632a573b0d4ef479742ed7354aca19b
Valentin Heidelberger <valh@mailbox.org>
2023-06-24 16:09:08 +0200

    add yay config generation

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -4,6 +4,10 @@ echo "Upgrading packages and installing yay..."
 sudo pacman -Syu yay
 echo ""
 
+echo "Setting yay config..."
+yay -Y --cleanafter --save
+echo ""
+
 echo "Iterating package lists..."
 find pkgs/ -type f -name "*.list" -exec bash -c '
     read -n1 -p "Install packages from {} [y/n] " yn
