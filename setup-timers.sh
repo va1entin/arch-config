2838fa7019bcca629842fa7f02822c6d7c237a42
Valentin Heidelberger <valh@mailbox.org>
2024-08-30 12:43:52 +0200

    add shebang and logging to setup-timers.sh

--- setup-timers.sh
+++ setup-timers.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Enabling paccache timer..."
 sudo systemctl enable paccache.timer
+echo ""
+
+echo "Starting paccache timer..."
 sudo systemctl start paccache.timer
+echo ""
