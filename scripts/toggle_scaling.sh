8633a41bfba5241d55842bdd03465ca9c84158d1
Valentin Heidelberger <valh@mailbox.org>
2024-09-16 15:17:45 +0200

    toggle scaling based on xrandr query to avoid inconsistencies with automatically changed xfconf settings breaking simple toggle logic

--- scripts/toggle_scaling.sh
+++ scripts/toggle_scaling.sh
@@ -17,32 +17,24 @@ while getopts ":r" opt; do
     esac
 done
 
-# Window scaling factor
-window_scaling_factor=$(xfconf-query -c xsettings -p /Gdk/WindowScalingFactor)
-if [ $window_scaling_factor -eq 1 ]; then
+display_connected=$(xrandr --query | grep -w connected | grep -v eDP-1)
+
+if [ -n "$display_connected" ]; then
     xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
     echo "Set window scaling factor to xhdpi default 2"
-else
-    xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 1
-    echo "Set window scaling factor to normal-dpi default 1"
-fi
 
-# xfwm theme
-xfwm_theme=$(xfconf-query -c xfwm4 -p /general/theme)
-if [ $xfwm_theme == "Default" ]; then
     xfconf-query -c xfwm4 -p /general/theme -s "Default-xhdpi"
     echo "Set xfwm theme to xhdpi default"
-else
-    xfconf-query -c xfwm4 -p /general/theme -s "Default"
-    echo "Set xfwm theme to normal-dpi default"
-fi
 
-# Mouse cursor size
-cursor_size=$(xfconf-query -c xsettings -p /Gtk/CursorThemeSize)
-if [ $cursor_size -eq 16 ]; then
     xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 42
     echo "Set cursor size to xhdpi default 42"
 else
+    xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 1
+    echo "Set window scaling factor to normal-dpi default 1"
+
+    xfconf-query -c xfwm4 -p /general/theme -s "Default"
+    echo "Set xfwm theme to normal-dpi default"
+
     xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 16
     echo "Set cursor size to normal-dpi default 16"
 fi
