3e623380d26ed1c422d2b60733de4f5cb17742c4
Valentin Heidelberger <valh@mailbox.org>
2024-12-16 10:50:40 +0100

    enable and start syncthing service in one go

--- setup-syncthing.sh
+++ setup-syncthing.sh
@@ -4,10 +4,6 @@ echo "Installing syncthing..."
 yay -S syncthing
 echo ""
 
-echo "Enabling syncthing user service..."
-systemctl enable --user syncthing.service
-echo ""
-
-echo "Starting syncthing user service..."
-systemctl start --user syncthing.service
+echo "Enabling and starting syncthing user service..."
+systemctl enable --now --user syncthing.service
 echo ""
