6aff920340842eabb5dd232eec956d38406a2055
Valentin Heidelberger <valh@mailbox.org>
2025-03-02 14:56:27 +0100

    add reflector and enable reflector.timer

--- setup-timers.sh
+++ setup-timers.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Enabling reflector timer..."
+sudo systemctl enable --now reflector.timer
+echo ""
+
 echo "Enabling yaycache timer..."
 sudo systemctl enable --now yaycache.timer
 echo ""
