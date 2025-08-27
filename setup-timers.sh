316d06a8e301b13e7528a8ed597bd18bb5adc9fc
Valentin Heidelberger <valh@mailbox.org>
2025-08-27 21:19:41 +0200

    add override to run reflector.timer daily instead of weekly

--- setup-timers.sh
+++ setup-timers.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# pacman / yay
+
+echo "Adding override to run reflector timer daily instead of weekly..."
+sudo mkdir -p /etc/systemd/system/reflector.timer.d
+echo "[Timer]
+OnCalendar=
+OnCalendar=daily" | sudo tee /etc/systemd/system/reflector.timer.d/override.conf > /dev/null
+sudo systemctl daemon-reload
+echo ""
+
 echo "Enabling reflector timer..."
 sudo systemctl enable --now reflector.timer
 echo ""
