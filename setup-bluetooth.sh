8c2bb0b8eab0503de0a526e2777584a91768f386
Valentin Heidelberger <valh@mailbox.org>
2025-10-31 12:54:35 +0100

    unblock bluetooth via rfkill and power on controller in setup-bluetooth.sh

--- setup-bluetooth.sh
+++ setup-bluetooth.sh
@@ -3,3 +3,11 @@
 echo "Enabling bluetooth service and starting it immediately..."
 systemctl enable --now bluetooth
 echo "Done!"
+
+echo "Unblocking bluetooth via rfkill..."
+rfkill unblock bluetooth
+echo "Done!"
+
+echo "Powering on bluetooth adapter..."
+bluetoothctl power on
+echo "Done!"
