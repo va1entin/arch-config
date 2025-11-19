38151134762e5e549c02dbb3159f94088e0fd9b3
Valentin Heidelberger <valh@mailbox.org>
2025-11-19 17:49:01 +0100

    connect_bluetooth_audio_device.sh: add usage, take name from wpctl status as parameter instead of hardcoded

--- scripts/connect_bluetooth_audio_device.sh
+++ scripts/connect_bluetooth_audio_device.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# connect_bluetooth_audio_device.sh <mac address> <device name as seen in wpctl status>
+
 bluetoothctl connect "$1"
 
 id=""
 while [ -z "$id" ]; do
-    id=$(wpctl status | grep -Eo '[0-9]*\. WH-1000XM4 .*vol' | sed 's/\..*//g')
+    id=$(wpctl status | grep -Eo "[0-9]*\. $2 .*vol" | sed 's/\..*//g')
     sleep 1
 done
 
